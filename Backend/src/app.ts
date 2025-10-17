6d0c683058620ba9c2dfa857904680959c38638f
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-10-17 14:56:03 +0530

    Solved some minor bugs and improved database connectivity code

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,21 +1,19 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-// import cors from "cors";
-import todoRouter from "./routers/todo.js";
-import userRouter from "./routers/users.js";
+import todoRouter from "./routers/todo";
+import userRouter from "./routers/users";
 
 const app=express();
 
 dotenv.config({path:'src/.env'});
-// app.use(cors(process.env.CLIENT_URL));
 app.use(express.json());
 
 app.use('/todos',todoRouter);
 app.use('/users',userRouter);
 
-await mongoose.connect(process.env.CONNECTION_STRING || '');
-
-app.listen(process.env.PORT,()=>{
-    console.log("Server Connected");
+mongoose.connect(process.env.CONNECTION_STRING || '').then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log("Server Connected");
+    });
 });
