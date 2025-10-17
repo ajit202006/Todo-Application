ba5d167ccea660f5bf60eefb6a91985467d1c5d1
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-10-17 14:48:01 +0530

    Added types and error handling

--- Backend/src/models/todo.ts
+++ Backend/src/models/todo.ts
@@ -1,39 +1,43 @@
 import User from "../database/users.js";
 
 let todoModel = {
-    createTask: async (userInfo) => {
+    createTask: async (userInfo: { id: string, task: string, isDone: boolean }) => {
         const result = await User.findById(userInfo.id);
-        const newTodos = [...result.todos, { task: userInfo.task, isDone: userInfo.isDone }];
-        await result.updateOne({ todos: newTodos })
-        return result._id;
-    },
-
-    retrieveTasks: async (userId) => {
-        const data = await User.findById(userId);;
-        return data.todos;
-    },
-
-    updateTask: async (id, task) => {
-        try {
-            const current_todo = await User.findById(id);
-            await current_todo.updateOne({ isDone: !current_todo.isDone });
-        } catch (error) {
-            console.log(error);
+        if (result) {
+            const newTodos = [...result.todos, { task: userInfo.task, isDone: userInfo.isDone }];
+            await result.updateOne({ todos: newTodos })
+            return result._id;
+        } else {
+            throw Error("User not found");
         }
     },
-    markDone: async ({ id: userId, index: index }) => {
-        const userData = await User.findById(userId);
-        const newTodos= [...userData.todos];
-        newTodos[index].isDone=!(newTodos[index].isDone);
-        await userData.updateOne({ todos: newTodos });
+
+    retrieveTasks: async (userId: string) => {
+        const data = await User.findById(userId);
+        return data?.todos;
     },
 
-    deleteTask: async ({ id: userId, index: index }) => {
+    markDone: async (userId: string, index: number) => {
         const userData = await User.findById(userId);
-        let newTodos = [...userData.todos];
-        newTodos.splice(index, 1);
-        await userData.updateOne({ todos: newTodos });
-        return true;
+        if (userData) {
+            const newTodos = [...userData.todos];
+            newTodos[index].isDone = !(newTodos[index].isDone);
+            await userData.updateOne({ todos: newTodos });
+        } else {
+            throw Error("User not found");
+        }
+    },
+
+    deleteTask: async (userId: string, index: number) => {
+        const userData = await User.findById(userId);
+        if (userData) {
+            let newTodos = [...userData.todos];
+            newTodos.splice(index, 1);
+            await userData.updateOne({ todos: newTodos });
+            return true;
+        } else {
+            throw Error("User not found.");
+        }
     }
 }
 
