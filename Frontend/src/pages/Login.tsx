c50795debdf00c9ff75665ca1dc52d37851608e6
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-11-10 17:40:43 +0530

    Created login page

---
+++ Frontend/src/pages/Login.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import LoginForm from "../components/LoginForm.tsx";
+import { useNavigate } from "react-router-dom";
+import Header from "../components/Header.tsx";
+
+const Login = () => {
+  const navigate=useNavigate();
+  if (!localStorage.length){
+    return (
+      <div className="flex w-screen h-screen bg-emerald-100 justify-center items-center">
+        <div className='md:w-2/3 md:h-6/7 w-screen h-screen bg-green-300 md:rounded-2xl relative overflow-hidden md:animate-scale'>
+          <Header />
+          <LoginForm />
+        </div>
+      </div>
+    )
+  }else{
+    useEffect(()=>{
+      navigate('/todos');
+    },[]);
+  }
+}
+
+export default Login
