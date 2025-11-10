21db9a4c5c3091013b70a644a3bbf9e2b57ca80c
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-11-10 17:51:06 +0530

    Added cors options

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,13 +1,20 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from 'cors'
 import mongoose from "mongoose";
 import todoRouter from "./routers/todo";
 import userRouter from "./routers/users";
 
 const app=express();
 
+const corsOptions = {
+    origin: [process.env.CLIENT_BASE_URL || 'http://localhost:5173'],
+    methods: 'GET,PUT,POST,DELETE',
+};
+
 dotenv.config({path:'src/.env'});
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use('/todos',todoRouter);
 app.use('/users',userRouter);
